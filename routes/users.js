ab3c77e464c22fc2bc04c527811f619f2c478c03
itbbt <teamitbbt@gmail.com>
2024-02-04 21:12:39 +0700

    feat: add route post

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var telegramBot = require("../helpers/telegram-bot");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/", function (req, res, next) {
+  const { chatId, message } = req.body;
+  telegramBot
+    .sendMessage(chatId, message)
+    .then((response) => {
+      console.log({ response });
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log({ err });
+      res.send(err);
+    });
 });
 
 module.exports = router;
